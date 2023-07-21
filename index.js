3a8ea7ef1f61a7800c98be16f795f96bfa43ea3b
Rowland Oka <rowlandoka@gmail.com>
2023-07-21 12:09:59 +0100

    Finished work and refactoring palindrome

--- index.js
+++ index.js
@@ -9,7 +9,27 @@ function Phrase(content) {
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
     return this.content.toLowerCase();
+    // return this.content;
   };
+
+  // Return the letters in the content
+  this.letters = function letters() {
+    let theLetters = [];
+    const letterRegex = /[a-z]/i;
+    Array.from(this.content).forEach(function (character) {
+      if (character.match(letterRegex)) {
+        theLetters.push(character);
+      }
+    });
+    return theLetters.join("");
+    // for (let i = 0; i < this.content.length; i++) {
+    //   let character = this.content.charAt(i);
+    //   if (character.match(letterRegex)) {
+    //     theLetters.push(character);
+    //   }
+    // }
+  };
+
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
     return this.processedContent() === this.processedContent().reverse();
