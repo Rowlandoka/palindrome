efdb9bd0d384f708448ee05160fbfa40c3c8b2a9
Rowland Oka <rowlandoka@gmail.com>
2023-07-21 12:24:01 +0100

    Finished work and refactoring palindrome

--- index.js
+++ index.js
@@ -1,37 +1,24 @@
 module.exports = Phrase;
+
 // Adds `reverse` to all strings.
 String.prototype.reverse = function () {
   return Array.from(this).reverse().join("");
 };
-// Defines a Phrase object.
+
 function Phrase(content) {
   this.content = content;
-  // Returns content processed for palindrome testing.
-  this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
-    // return this.content;
-  };
 
-  // Return the letters in the content
-  this.letters = function letters() {
-    let theLetters = [];
-    const letterRegex = /[a-z]/i;
-    Array.from(this.content).forEach(function (character) {
-      if (character.match(letterRegex)) {
-        theLetters.push(character);
-      }
-    });
-    return theLetters.join("");
-    // for (let i = 0; i < this.content.length; i++) {
-    //   let character = this.content.charAt(i);
-    //   if (character.match(letterRegex)) {
-    //     theLetters.push(character);
-    //   }
-    // }
+  // Return content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.content.match(/[a-z]/gi).join("").toLowerCase();
   };
 
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
-    return this.processedContent() === this.processedContent().reverse();
+    if (this.content) {
+      return this.processedContent() === this.processedContent().reverse();
+    } else {
+      return false;
+    }
   };
 }
--- test/test.js
+++ test/test.js
@@ -4,26 +4,28 @@ let Phrase = require("../index.js");
 describe("Phrase", function () {
   describe("#palindrome", function () {
     it("should return false for a non-palindrome", function () {
-      let nonPalindrome = new Phrase("apple");
+      let nonPalindrome = new Phrase("foobar");
       assert(!nonPalindrome.palindrome());
     });
-    it("should return true for a palindrome", function () {
+
+    it("should return true for a plain palindrome", function () {
       let plainPalindrome = new Phrase("racecar");
       assert(plainPalindrome.palindrome());
     });
-    it("should return true for a mixed-cased palindrome", function () {
-      let mixedPalindrome = new Phrase("RacecaR");
-      assert(mixedPalindrome.palindrome());
+
+    it("should return true for a mixed-case palindrome", function () {
+      let combinedCase = new Phrase("RaceCar");
+      assert(combinedCase.palindrome());
     });
-  });
-  describe("#letters", function () {
-    it("should return only letters", function () {
+
+    it("should return true for a palindrome with punctuation", function () {
       let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
-      assert.strictEqual(punctuatedPalindrome.letters(), "MadamImAdam");
+      assert(punctuatedPalindrome.palindrome());
     });
-    it("should return the empty string on no match", function () {
-      let noLetters = new Phrase("1234.56");
-      assert.strictEqual(noLetters.letters(), "");
+
+    it("should return false for an empty string", function () {
+      let emptyPhrase = new Phrase("");
+      assert(!emptyPhrase.palindrome());
     });
   });
 });
